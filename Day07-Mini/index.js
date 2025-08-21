3f62a06925840e0579e818eab83b1866495c4e5b
Musya-Lungi <stevemusya123@gmail.com>
2025-08-21 12:36:03 +0300

    js variables

--- Day07-Mini/index.js
+++ Day07-Mini/index.js
@@ -2,4 +2,3 @@ document.writeln("Hello, World!");
 document.writeln("This is a simple JavaScript file.");
 document.writeln("You can include this file in your HTML to display messages.");
 document.writeln("Make sure to link it properly in your HTML file.");   
-s
