fd40966804c8e02b833d9d93681029f1b16cc490
Musya-Lungi <stevemusya123@gmail.com>
2025-08-21 10:36:44 +0300

    Functions and scope in js

---
+++ Day02-functions-scope/index.js
@@ -0,0 +1,9 @@
+document.writeln("Hello, World!");
+document.writeln("This is a simple JavaScript file.");
+document.writeln("You can include this file in your HTML to display messages.");
+document.writeln("Make sure to link it properly in your HTML file.");
+document.writeln("Enjoy coding with JavaScript!");
+document.writeln("Remember to check the console for any errors.");
+document.writeln("Happy coding!");
+document.writeln("Feel free to modify this code as you wish.");
+document.writeln("This is the end of the script."); 
