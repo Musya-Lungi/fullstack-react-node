3c878bb780b5a991562c00716ca7d7390bb36720
Musya-Lungi <stevemusya123@gmail.com>
2025-08-21 11:00:13 +0300

    js mini project

---
+++ Day07-Mini/index.js
@@ -0,0 +1,5 @@
+document.writeln("Hello, World!");
+document.writeln("This is a simple JavaScript file.");
+document.writeln("You can include this file in your HTML to display messages.");
+document.writeln("Make sure to link it properly in your HTML file.");   
+s
